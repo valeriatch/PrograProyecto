14e1d58023fc788f1905bda0e34fb8a1f53ff965
JeremyCP98 <53958170+JeremyCP98@users.noreply.github.com>
2020-09-25 18:08:47 -0600

    agregacion clase aerolinea

---
+++ ProyectoProgra/src/proyectoprogra/modelo/Aerolinea.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogra.modelo;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class Aerolinea {
+    private String nombre;
+    private int fecha;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(int fecha) {
+        this.fecha = fecha;
+    }
+    
+    public Aerolinea(){
+        nombre = "";
+        fecha = 0;
+    }
+}
