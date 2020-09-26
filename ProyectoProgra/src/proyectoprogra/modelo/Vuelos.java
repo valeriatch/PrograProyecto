0a9eb495ca041e3082d2c8e26ae2686058ab2b10
JosueA14 <46728032+JosueA14@users.noreply.github.com>
2020-09-25 21:04:37 -0600

    Vuelos

---
+++ ProyectoProgra/src/proyectoprogra/modelo/Vuelos.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoprogra.modelo;
+
+/**
+ *
+ * @author XperriX
+ */
+public class Vuelos {
+
+    public Aerolinea getAerolinea() {
+        return aerolinea;
+    }
+
+    public void setAerolinea(Aerolinea aerolinea) {
+        this.aerolinea = aerolinea;
+    }
+
+    public String getNumeroVuelo() {
+        return numeroVuelo;
+    }
+
+    public void setNumeroVuelo(String numeroVuelo) {
+        this.numeroVuelo = numeroVuelo;
+    }
+
+    public String getSalida() {
+        return salida;
+    }
+
+    public void setSalida(String salida) {
+        this.salida = salida;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getHorarioSalida() {
+        return horarioSalida;
+    }
+
+    public void setHorarioSalida(String horarioSalida) {
+        this.horarioSalida = horarioSalida;
+    }
+
+    public String getHorarioLlegada() {
+        return horarioLlegada;
+    }
+
+    public void setHorarioLlegada(String horarioLlegada) {
+        this.horarioLlegada = horarioLlegada;
+    }
+
+    public String getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(String fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public int getDuracionVuelo() {
+        return duracionVuelo;
+    }
+
+    public void setDuracionVuelo(int duracionVuelo) {
+        this.duracionVuelo = duracionVuelo;
+    }
+
+    public int getNumeroAsientos() {
+        return numeroAsientos;
+    }
+
+    public void setNumeroAsientos(int numeroAsientos) {
+        this.numeroAsientos = numeroAsientos;
+    }
+    
+    private Aerolinea aerolinea;
+    private String numeroVuelo;
+    private String salida;
+    private String destino;
+    private String horarioSalida;
+    private String horarioLlegada;
+    private String fechaCreacion;
+    private int duracionVuelo;
+    private int numeroAsientos;
+    
+}
