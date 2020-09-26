3388d56330a1d09bfe51ff5aabec38eb990f3277
JeremyCP98 <53958170+JeremyCP98@users.noreply.github.com>
2020-09-26 17:01:24 -0600

    eliminacion

--- ProyectoProgra/src/proyectoprogra/controlador/Controlador.java
+++ ProyectoProgra/src/proyectoprogra/controlador/Controlador.java
@@ -29,7 +29,7 @@ public class Controlador implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
 }
